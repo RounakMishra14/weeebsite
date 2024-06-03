a74c84c8411298604e6787aefd1b61394dfa7bbc
RounakMishra14 <129485729+RounakMishra14@users.noreply.github.com>
2024-06-03 16:08:40 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,17 +1,42 @@
 document.getElementById('infoForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    fetch('https://api.ipify.org?format=json')
-        .then(response => response.json())
-        .then(data => {
-            const ip = data.ip;
-            document.getElementById('ip').value = ip;
+    const userAgent = navigator.userAgent;
+    document.getElementById('userAgent').value = userAgent;
 
-            fetch(`https://ipinfo.io/${ip}/geo`)
-                .then(response => response.json())
-                .then(data => {
-                    document.getElementById('location').value = data.loc;
-                    document.getElementById('infoForm').submit();
-                });
-        });
+    const screenWidth = screen.width;
+    const screenHeight = screen.height;
+    const availWidth = screen.availWidth;
+    const availHeight = screen.availHeight;
+    const colorDepth = screen.colorDepth;
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    document.getElementById('screenWidth').value = screenWidth;
+    document.getElementById('screenHeight').value = screenHeight;
+    document.getElementById('availWidth').value = availWidth;
+    document.getElementById('availHeight').value = availHeight;
+    document.getElementById('colorDepth').value = colorDepth;
+    document.getElementById('timezone').value = timezone;
+
+    // Collect battery status
+    navigator.getBattery().then(function(battery) {
+        const batteryLevel = battery.level * 100;
+        const isCharging = battery.charging;
+        document.getElementById('batteryLevel').value = batteryLevel;
+        document.getElementById('isCharging').value = isCharging;
+
+        fetch('https://api.ipify.org?format=json')
+            .then(response => response.json())
+            .then(data => {
+                const ip = data.ip;
+                document.getElementById('ip').value = ip;
+
+                fetch(`https://ipinfo.io/${ip}/geo`)
+                    .then(response => response.json())
+                    .then(data => {
+                        document.getElementById('location').value = data.loc;
+                        document.getElementById('infoForm').submit();
+                    });
+            });
+    });
 });
